dc02cc888a59389437952915be1432606568bbcb
Gerhard Pegel <gpegel@gmail.com>
2017-05-19 22:16:41 +0200

    Initial upload Android Studio Project

---
+++ math_app_for_kids/app/src/main/java/nl/gerhardpegel/math_app_for_kids/MainActivity.java
@@ -0,0 +1,13 @@
+package nl.gerhardpegel.math_app_for_kids;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

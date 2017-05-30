19d4d3679d9979707bcd8f88434890a6ca6de587
Gerhard Pegel <gpegel@gmail.com>
2017-05-30 21:41:55 +0200

    App is working and in Beta stage

---
+++ math_app_for_kids/app/src/androidTest/java/nl/gerhardpegel/math_app_for_kids/MainActivityTest.java
@@ -0,0 +1,10 @@
+package nl.gerhardpegel.math_app_for_kids;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by gerhard on 30-5-17.
+ */
+public class MainActivityTest {
+
+}
--- math_app_for_kids/app/src/main/java/nl/gerhardpegel/math_app_for_kids/MainActivity.java
+++ math_app_for_kids/app/src/main/java/nl/gerhardpegel/math_app_for_kids/MainActivity.java
@@ -1,33 +1,54 @@
 package nl.gerhardpegel.math_app_for_kids;
 
-import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import nl.gerhardpegel.math_app_for_kids.R;
 
+public class MainActivity extends Activity implements OnClickListener {
 
-public class MainActivity extends Activity implements OnClickListener{
+    //ui items
     private Button playBtn, helpBtn, highBtn;
+    //level names
+    String[] levelNames = {"Easy", "Medium", "Hard"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //retrieve references
         playBtn = (Button)findViewById(R.id.play_btn);
         helpBtn = (Button)findViewById(R.id.help_btn);
         highBtn = (Button)findViewById(R.id.high_btn);
 
+        //listen for clicks
+        playBtn.setOnClickListener(this);
+        helpBtn.setOnClickListener(this);
+        highBtn.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-//respond to clicks
-
         if(view.getId()==R.id.play_btn){
             //play button
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Choose a level")
+                    .setSingleChoiceItems(levelNames, 0, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                            //start gameplay
+                            startPlay(which);
+                        }
+                    });
+            AlertDialog ad = builder.create();
+            ad.show();
         }
         else if(view.getId()==R.id.help_btn){
             //how to play button
@@ -36,4 +57,19 @@ public class MainActivity extends Activity implements OnClickListener{
             //high scores button
         }
     }
+
+    private void startPlay(int chosenLevel){
+        //start gameplay
+        Intent playIntent = new Intent(this, PlayGame.class);
+        playIntent.putExtra("level", chosenLevel);
+        this.startActivity(playIntent);
+    }
+/*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    */
 }
